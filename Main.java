b76863c5d3c097198192ca1118a91e90b9e445f7
bazzl1202 <71136617+bazzl1202@users.noreply.github.com>
2020-10-02 10:52:07 -0400

    Update Main.java
    Asks for a number and counts down by 5

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Asks for a number and counts down by 5
+ * @author Liana Bazzarella
  */
 public class Main {
 
@@ -9,7 +10,26 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a Scanner for user input
+    Scanner input = new Scanner(System.in);
+
+   // ask the user for an intger
+   System.out.println("Please enter an integer to count down to");
+   int integer = input.nextInt();
+
+   // declare and initilaze variable you count down from
+   int count = 100;
+
+   // tell user the count down is starting
+   System.out.println("Count Down:");
+
+   while(integer <= count){
+     // print the number we are at 
+     System.out.println(count);
+     // decrease the value of the number by 5
+     count = count - 5;
+     
+   }
     
   }
 }
